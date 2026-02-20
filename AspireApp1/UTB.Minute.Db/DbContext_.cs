eddf75eee08a23e32ae16970db0cb0d1583bc192
Hermedita <eva.hermanova@hotmail.com>
2026-02-20 11:06:23 +0100

    db

--- AspireApp1/UTB.Minute.Db/DbContext_.cs
+++ AspireApp1/UTB.Minute.Db/DbContext_.cs
@@ -4,5 +4,5 @@ namespace UTB.Minute.Db;
 
 public class DbContext_(DbContextOptions< DbContext_ >options): DbContext(options)
 {
-    public DbSet<Entity> Entities { get; set; }
+    public DbSet<Entity> Entities { get; set; } //tabulky v databazi
 }
