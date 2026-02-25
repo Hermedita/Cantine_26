31cd3fd08d96c1906d4eaac13fa9c2f67cd7ffd2
Hermedita <eva.hermanova@hotmail.com>
2026-02-25 14:48:19 +0100

    meal db
    base db - zmeni se

--- AspireApp1/UTB.Minute.Db/DbContext_.cs
+++ AspireApp1/UTB.Minute.Db/DbContext_.cs
@@ -4,5 +4,5 @@ namespace UTB.Minute.Db;
 
 public class DbContext_(DbContextOptions< DbContext_ >options): DbContext(options)
 {
-    public DbSet<Meals> Meal { get; set; } //tabulky v databazi
+    public DbSet<Meal> Meal { get; set; } //tabulky v databazi
 }
