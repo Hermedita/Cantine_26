ec53c3e0a3ba00317516e1ad2afcb4f69166f129
Hermedita <eva.hermanova@hotmail.com>
2026-02-27 18:13:22 +0100

    db add

--- AspireApp1/UTB.Minute.Db/DbContext_.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore;
-
-namespace UTB.Minute.Db;
-
-public class DbContext_(DbContextOptions< DbContext_ >options): DbContext(options)
-{
-    public DbSet<Meal> Meal { get; set; } //tabulky v databazi
-}
---
+++ AspireApp1/UTB.Minute.Db/MealDbContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace UTB.Minute.Db;
+
+
+class Program
+{
+    static void Main(){
+
+        var options = new DbContextOptionsBuilder<MealContext>()
+                .UseSqlServer("Data Source=meals.db")
+                .Options;
+
+        using var context = new MealContext(options);
+        context.Database.Migrate();
+    }
+    public class MealContext : DbContext{}
+
+    List<Meal> meals =
+    [
+        new Meal()
+        {
+            Id = 1,
+            Name = "Kurizek",
+            Description = "Mnam",
+            Price = 100,
+            IsAvailable = true
+        },
+        new Meal()
+        {
+            Id = 2,
+            Name = "Svickova",
+            Description = "Knedlik je gud",
+            Price = 89,
+            IsAvailable = false
+        }
+    ];
+
+    public class MealContext(DbContextOptions<MealContext> options) : DbContext(options)
+    {
+        public DbSet<Meal> Meals { get; set; } //tabulky v databazi
+        public DbSet<Order> Orders { get; set; }
+    }
+}
+
+
