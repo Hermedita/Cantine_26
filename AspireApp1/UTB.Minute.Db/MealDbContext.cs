092845905a2d472b5e0aa65aa0261736a26cb74b
Hermedita <eva.hermanova@hotmail.com>
2026-03-06 11:24:09 +0100

    oprava mealcontext
    svitilo to cervene

--- AspireApp1/UTB.Minute.Db/MealDbContext.cs
+++ AspireApp1/UTB.Minute.Db/MealDbContext.cs
@@ -4,42 +4,51 @@ namespace UTB.Minute.Db;
 
 class Program
 {
-    static void Main(){
+    static void Main()
+    {
 
         var options = new DbContextOptionsBuilder<MealContext>()
-                .UseSqlServer("Data Source=meals.db")
-                .Options;
+            .UseSqlServer("Data Source=meals.db")
+            .Options;
 
         using var context = new MealContext(options);
         context.Database.Migrate();
+        // }
+        // public class MealContext : DbContext{}
+
+        List<Meal> meals =
+        [
+            new Meal()
+            {
+                Id = 1,
+                Name = "Kurizek",
+                Description = "Mnam",
+                Price = 100,
+                IsAvailable = true
+            },
+            new Meal()
+            {
+                Id = 2,
+                Name = "Svickova",
+                Description = "Knedlik je gud",
+                Price = 89,
+                IsAvailable = false
+            }
+        ];
+        context.Meals.AddRange(meals);
+        context.SaveChanges();
     }
-    public class MealContext : DbContext{}
 
-    List<Meal> meals =
-    [
-        new Meal()
-        {
-            Id = 1,
-            Name = "Kurizek",
-            Description = "Mnam",
-            Price = 100,
-            IsAvailable = true
-        },
-        new Meal()
-        {
-            Id = 2,
-            Name = "Svickova",
-            Description = "Knedlik je gud",
-            Price = 89,
-            IsAvailable = false
-        }
-    ];
-
-    public class MealContext(DbContextOptions<MealContext> options) : DbContext(options)
+    public class MealContext : DbContext
     {
+        public MealContext(DbContextOptions<MealContext> options) : base (options)
+        {
+        }
+
         public DbSet<Meal> Meals { get; set; } //tabulky v databazi
         public DbSet<Order> Orders { get; set; }
+        }
     }
-}
+
 
 
