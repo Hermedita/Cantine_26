26e6c78de6652e2d02f05cc2432a55f7876d75ee
tomas45100 <tom.prik45@gmail.com>
2026-05-27 12:38:14 +0200

    sse opraveno

---
+++ AspireApp1/UTB.Minute.WebApi/SseNotificationService.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Concurrent;
+
+public class SseNotificationService
+{
+    private readonly ConcurrentBag<StreamWriter> _clients = new();
+
+    public void AddClient(StreamWriter client)
+    {
+        _clients.Add(client);
+    }
+
+    public async Task BroadcastOrderUpdateAsync(OrderUpdateMessage message)
+    {
+        var json = System.Text.Json.JsonSerializer.Serialize(message);
+        var activeClients = _clients.ToList();
+
+        foreach (var client in activeClients)
+        {
+            try
+            {
+                await client.WriteAsync($"data: {json}\n\n");
+                await client.FlushAsync();
+            }
+            catch
+            {
+                // Pokud klient neodpovídá (zavřel prohlížeč), v reálném produkčním kódu by se měl odstranit.
+                // Pro zjednodušení školního projektu to stačí takto, případně streamy čistit.
+            }
+        }
+    }
+}
+
+public class OrderUpdateMessage
+{
+    public int TotalPortions { get; set; }
+    public decimal TotalPrice { get; set; }
+}
